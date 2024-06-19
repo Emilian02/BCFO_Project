4e0c2df0da4cdd7b924828807956984fa51e1250
Dayana Cruz <115519468+CodingCamu@users.noreply.github.com>
2024-06-18 21:22:38 -0700

    Player Helath bar done

--- BCFO_Project/Assets/Scripts/Player/PlayerHealth.cs
+++ BCFO_Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float currenthealth;
+    public Slider healthSlider;
+    public Gradient gradient;
+    public Image sliderFill;
 
     void Start()
     {
         currenthealth = health;
+        healthSlider.maxValue = health;
+        sliderFill.color = gradient.Evaluate(1f);
     }
 
     // Update is called once per frame
@@ -18,11 +24,22 @@ public class PlayerHealth : MonoBehaviour
         if (currenthealth < health)
         {
             currenthealth = health;
+            
 
         }
         if (health <= 0)
         {
             Debug.Log("L bozo");
         }
+        SetHealth(currenthealth);
+    }
+
+    public void SetHealth(float health)
+    {
+        
+        healthSlider.value = health;
+
+        sliderFill.color = gradient.Evaluate(healthSlider.normalizedValue);
+
     }
 }
