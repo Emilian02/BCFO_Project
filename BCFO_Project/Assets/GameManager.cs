e3dbc163fd13d0a6d11b0e2131d8fc1927f4d0a5
Dayana Cruz <115519468+CodingCamu@users.noreply.github.com>
2024-06-18 18:36:39 -0700

    Change in Map

--- BCFO_Project/Assets/GameManager.cs
+++ BCFO_Project/Assets/GameManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool levelCompleted;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
