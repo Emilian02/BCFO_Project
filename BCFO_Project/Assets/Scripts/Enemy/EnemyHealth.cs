ffe4266a27d40f847ceb208bda7eefef90849f3e
Dayana Cruz <115519468+CodingCamu@users.noreply.github.com>
2024-06-19 00:41:25 -0700

    Enemy health prefab ready

--- BCFO_Project/Assets/Scripts/Enemy/EnemyHealth.cs
+++ BCFO_Project/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float health;
     public float currenthealth;
+    public Slider healthSlider;
+    public Gradient gradient;
+    public Image sliderFill;
+
     [SerializeField] public static bool isBoss;
 
     void Start()
     {
         currenthealth = health;
+        healthSlider.maxValue = health;
+        sliderFill.color = gradient.Evaluate(1f);
     }
 
     void Update()
@@ -29,5 +36,15 @@ public class EnemyHealth : MonoBehaviour
         {
             Debug.Log("Enemy Down");
         }
+        SetHealth(currenthealth);
+    }
+
+    public void SetHealth(float health)
+    {
+
+        healthSlider.value = health;
+
+        sliderFill.color = gradient.Evaluate(healthSlider.normalizedValue);
+
     }
 }
